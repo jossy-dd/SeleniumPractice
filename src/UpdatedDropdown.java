c5d5ceeb08c2ed550c9b45191e26fd3a7f747892
Dinora Alvarez <jossdin.alvarez@gmail.com>
2025-01-17 18:14:13 -0600

    working with auto suggestive dropdown list and checkboxes

--- src/UpdatedDropdown.java
+++ src/UpdatedDropdown.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdatedDropdown {
 
@@ -9,8 +10,16 @@ public class UpdatedDropdown {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		//driver.get("https://www.spicejet.com/");
+		//Verify the checkbox is NOT selected
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		//Verify if the checkbox is selected
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		//Count and print number of checkboxes
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		//dropdown list
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
-		driver.findElement(By.id("divpaxinfo")).click();//dropdown list
+		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000L);
 		int i = 1;
 		while(i<5) {
@@ -18,6 +27,7 @@ public class UpdatedDropdown {
 			i++;
 		}
 		driver.findElement(By.id("btnclosepaxoption")).click();
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
 }
